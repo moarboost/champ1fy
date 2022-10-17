d482ef0c26ea1663138e07c644a305d159d930c3
br1ght <br1ght@br1ghts-MacBook-Pro.local>
2022-10-17 10:02:48 -0400

    added more files

--- Champ1fy/asgi.py
+++ Champ1fy/asgi.py
@@ -1,16 +1,7 @@
-"""
-ASGI config for Champ1fy project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Champ1fy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'champify_project.settings')
 
 application = get_asgi_application()
