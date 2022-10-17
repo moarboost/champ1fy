d1853772cec64b839510ae696d0dd094545d6831
br1ght <br1ght@br1ghts-MacBook-Pro.local>
2022-10-17 09:59:17 -0400

    added css and app.js

---
+++ Champ1fy/static/scripts/app.js
@@ -0,0 +1,11 @@
+$(".navbar-burger").click(function () {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+  
+  console.log("Hi everyone");
+  
+  $(".dropdown").click(function (event) {
+    $(this).toggleClass("is-active");
+  });
+  
