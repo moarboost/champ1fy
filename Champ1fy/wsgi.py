63140e6ed775188164017bccffe7db32a51fa46b
br1ght <br1ght@br1ghts-MacBook-Pro.local>
2022-10-17 10:23:53 -0400

    added dependencies

--- Champ1fy/wsgi.py
+++ Champ1fy/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'champify_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Champ1fy.settings')
 
 application = get_wsgi_application()
